edf3e8f5390dbe7cb746b5eb60f6074d47d756fd
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-04-08 16:34:58 +0000

    递交底层通讯包

--- trunk/commons/src/net/roboduino/commons/util/ProtocolUtils.java
+++ trunk/commons/src/net/roboduino/commons/util/ProtocolUtils.java
@@ -1,8 +1,12 @@
 package net.roboduino.commons.util;
 
 public class ProtocolUtils {
-	public static byte buildChecksum(int data) {
-		return 0;
+	public static byte buildChecksum(int sum) {
+		if (sum > 0x00ff) {
+			sum += 1;
+		}
+		sum = sum & 0x00ff;
+		return (byte) sum;
 	}
 
 	public static boolean validateChecksum() {
