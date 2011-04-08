ac7684f4c725b6466d462d7bdf8e8a44beee02e9
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-04-08 16:34:58 +0000

    递交底层通讯包

--- commons/src/net/roboduino/commons/codec/BaseMsg.java
+++ commons/src/net/roboduino/commons/codec/BaseMsg.java
@@ -9,11 +9,13 @@ import org.slf4j.LoggerFactory;
 
 public abstract class BaseMsg {
 	private static final Logger logger = LoggerFactory.getLogger(BaseMsg.class);
-	private byte[] header;
+	private byte[] header = new byte[2];
 	private byte deviceAddress;
-	// private byte frameLen;
+	private byte frameLen;
 	private byte cmdType;
 	private byte[] content = {};
+	private byte sum;
+	private byte end;
 
 	public BaseMsg(byte cmdType, Class<? extends BaseMsg> clazz) {
 		this.cmdType = cmdType;
@@ -31,18 +33,87 @@ public abstract class BaseMsg {
 		buffer.put(this.getLength());
 		buffer.put(cmdType);
 		buffer.put(content);
-		int temp = 0;
+		short temp = 0;
 		for (byte e : content) {
-			temp += e;
+			temp += e & 0xff;
 		}
-		buffer.put(ProtocolUtils
-				.buildChecksum(ProtocolConstant.MSG_DEVICEADDRESS
-						+ this.getLength() + cmdType + temp));
+
+		sum = ProtocolUtils.buildChecksum(ProtocolConstant.MSG_DEVICEADDRESS
+				+ this.getLength() + cmdType + temp);
+		// buffer.getUnsigned()
+
+		buffer.put(sum);
 		buffer.put(ProtocolConstant.MSG_STOP);
 	}
 
 	public BaseMsg deserialize(IoBuffer buffer) {
-		return null;
+		//logger.info("");
+		header[0] = buffer.get();
+		header[1] = buffer.get();
+		deviceAddress = buffer.get();
+		frameLen = buffer.get();
+		cmdType = buffer.get();
+		content = new byte[frameLen];
+		buffer.get(content);
+		sum = buffer.get();
+		end = buffer.get();
+		return this;
+	}
+
+	public byte[] getHeader() {
+		return header;
+	}
+
+	public void setHeader(byte[] header) {
+		this.header = header;
+	}
+
+	public byte getDeviceAddress() {
+		return deviceAddress;
+	}
+
+	public void setDeviceAddress(byte deviceAddress) {
+		this.deviceAddress = deviceAddress;
+	}
+
+	public byte getFrameLen() {
+		return frameLen;
+	}
+
+	public void setFrameLen(byte frameLen) {
+		this.frameLen = frameLen;
+	}
+
+	public byte getCmdType() {
+		return cmdType;
+	}
+
+	public void setCmdType(byte cmdType) {
+		this.cmdType = cmdType;
+	}
+
+	public byte[] getContent() {
+		return content;
+	}
+
+	public void setContent(byte[] content) {
+		this.content = content;
+	}
+
+	public byte getSum() {
+		return sum;
+	}
+
+	public void setSum(byte sum) {
+		this.sum = sum;
+	}
+
+	public byte getEnd() {
+		return end;
+	}
+
+	public void setEnd(byte end) {
+		this.end = end;
 	}
 
 }
