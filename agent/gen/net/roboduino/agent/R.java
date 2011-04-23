b9d8bd8b540a85f82143e5625c26e80cf5bdbca2
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-04-23 19:49:05 +0000

    robot agent setting 模块的设计

--- agent/gen/net/roboduino/agent/R.java
+++ agent/gen/net/roboduino/agent/R.java
@@ -14,23 +14,28 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int editText1=0x7f060002;
-        public static final int spinner1=0x7f060001;
-        public static final int toggleButton4Enable=0x7f060000;
+        public static final int SETTING=0x7f060000;
+        public static final int UPDATE=0x7f060001;
+        public static final int checkBox1=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int apply_bluetooth=0x7f050003;
-        public static final int apply_bluetooth_summary=0x7f050004;
-        public static final int bluetooth_discoverable=0x7f050006;
-        public static final int bluetooth_discoverable_summary=0x7f050007;
-        public static final int bluetooth_name=0x7f050005;
-        public static final int bluetooth_scan=0x7f050008;
-        public static final int bluetooth_setting=0x7f050002;
+        public static final int apply_bluetooth=0x7f050004;
+        public static final int apply_bluetooth_summary=0x7f050005;
+        public static final int bluetooth_device_list=0x7f05000a;
+        public static final int bluetooth_discoverable=0x7f050007;
+        public static final int bluetooth_discoverable_summary=0x7f050008;
+        public static final int bluetooth_name=0x7f050006;
+        public static final int bluetooth_scan=0x7f050009;
+        public static final int bluetooth_setting=0x7f050003;
+        public static final int closing=0x7f05000d;
+        public static final int opening=0x7f05000c;
         public static final int setting=0x7f050001;
+        public static final int update=0x7f050002;
+        public static final int wifi_setting=0x7f05000b;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
