7f05bed67ae41dcc4feb24abcafdc7b76052a46d
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-05-07 11:44:32 +0000

    新增视频模块

---
+++ trunk/robotagent/src/net/roboduino/agent/Preview.java
@@ -0,0 +1,111 @@
+package net.roboduino.agent;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class Preview extends SurfaceView implements SurfaceHolder.Callback {
+	private static final Logger logger = LoggerFactory.getLogger(Preview.class);
+	private SurfaceHolder holder;
+	private Camera camera;
+
+	Preview(Context context) {
+		this(context, (AttributeSet) null);
+	}
+
+	public Preview(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// Install a SurfaceHolder.Callback so we get notified when the
+		// underlying surface is created and destroyed.
+		holder = getHolder();
+		holder.addCallback(this);
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		// The Surface has been created, acquire the camera and tell it where
+		// to draw.
+		camera = Camera.open();
+		try {
+			camera.setPreviewDisplay(holder);
+		} catch (IOException e) {
+			camera.release();
+			camera = null;
+			logger.error(e.getMessage(), e);
+			// TODO: add more exception handling logic here
+		}
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// Surface will be destroyed when we return, so stop the preview.
+		// Because the CameraDevice object is not a shared resource, it's very
+		// important to release it when the activity is paused.
+		camera.stopPreview();
+		camera.release();
+		camera = null;
+	}
+
+	private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+		final double ASPECT_TOLERANCE = 0.05;
+		double targetRatio = (double) w / h;
+		if (sizes == null)
+			return null;
+
+		Size optimalSize = null;
+		double minDiff = Double.MAX_VALUE;
+
+		int targetHeight = h;
+
+		// Try to find an size match aspect ratio and size
+		for (Size size : sizes) {
+			double ratio = (double) size.width / size.height;
+			if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+				continue;
+			if (Math.abs(size.height - targetHeight) < minDiff) {
+				optimalSize = size;
+				minDiff = Math.abs(size.height - targetHeight);
+			}
+		}
+
+		// Cannot find the one match the aspect ratio, ignore the requirement
+		if (optimalSize == null) {
+			minDiff = Double.MAX_VALUE;
+			for (Size size : sizes) {
+				if (Math.abs(size.height - targetHeight) < minDiff) {
+					optimalSize = size;
+					minDiff = Math.abs(size.height - targetHeight);
+				}
+			}
+		}
+		return optimalSize;
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// Now that the size is known, set up the camera parameters and begin
+		// the preview.
+		/* 构建Camera.Parameters对相机的参数进行设置 */
+		Camera.Parameters parameters = camera.getParameters();
+		/* 设置拍照的图片格式 */
+		parameters.setPictureFormat(PixelFormat.JPEG);
+		List<Size> sizes = parameters.getSupportedPreviewSizes();
+		Size optimalSize = getOptimalPreviewSize(sizes, width, height);
+		/* 设置Preview的尺寸 */
+		parameters.setPreviewSize(optimalSize.width, optimalSize.height);
+		/* 设置相机采用parameters */
+		camera.setParameters(parameters);
+		/* 开始预览 */
+		camera.startPreview();
+	}
+
+}
