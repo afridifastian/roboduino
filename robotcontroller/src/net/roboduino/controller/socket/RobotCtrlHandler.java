416fed269e48231e4e2fbea119cae2f5c409e0e4
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-05-09 16:26:52 +0000

    客户端socket完成

---
+++ robotcontroller/src/net/roboduino/controller/socket/RobotCtrlHandler.java
@@ -0,0 +1,54 @@
+package net.roboduino.controller.socket;
+
+import net.roboduino.commons.BaseMsg;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RobotCtrlHandler extends IoHandlerAdapter {
+	private static final Logger logger = LoggerFactory
+			.getLogger(RobotCtrlHandler.class);
+	private long time;
+
+	public void sessionCreated(IoSession session) {
+
+	}
+
+	/** 分发到各个功能界面 */
+	public void dispatch(IoSession iosession, BaseMsg msg) {
+
+	}
+
+	public void messageReceived(IoSession iosession, Object obj) {
+		BaseMsg msg = (BaseMsg) obj;
+		this.dispatch(iosession, msg);
+	}
+
+	public void sessionOpened(IoSession iosession) {
+		time = System.currentTimeMillis();
+
+	}
+
+	public void sessionClosed(IoSession iosession) {
+		time = System.currentTimeMillis() - time;
+		logger.info("Session Closed!keep time:{}ms", time);
+
+	}
+
+	public void exceptionCaught(IoSession session, Throwable cause)
+
+	{
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			logger.error(e.getMessage(), e);
+		}
+
+	}
+
+	public void messageSent(IoSession iosession, Object obj) {
+
+	}
+}
