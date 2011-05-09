8a206f39f4e5e944817c002359f4d5807599d567
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-05-09 16:26:52 +0000

    客户端socket完成

---
+++ trunk/robotcontroller/src/net/roboduino/controller/socket/Client.java
@@ -0,0 +1,61 @@
+package net.roboduino.controller.socket;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.concurrent.TimeUnit;
+
+import net.roboduino.commons.BaseMsg;
+import net.roboduino.commons.CodecFactory;
+
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+	private static final Logger logger = LoggerFactory.getLogger(Client.class);
+	private static NioSocketConnector connector;
+	private static IoSession session;
+
+	public static void init() {
+		connector = new NioSocketConnector();
+		connector.getFilterChain().addLast("codec",
+				new ProtocolCodecFilter(new CodecFactory()));
+		connector.setHandler(new RobotCtrlHandler());
+		connector.getSessionConfig().setReuseAddress(true);
+	}
+
+	public static void connect(String ip, int port) throws InterruptedException {
+		SocketAddress remoteAddress = new InetSocketAddress(ip, port);
+		logger.info("Try to connect {}:{}", ip, port);
+		boolean isConnect = false;
+		int i = 0;
+		while (!isConnect && i < 6) {
+			logger.info("Time:{}", (++i));
+			ConnectFuture connectFuture = connector.connect(remoteAddress);
+			connectFuture.await(2000, TimeUnit.MILLISECONDS);
+			Throwable exception = connectFuture.getException();
+			if (exception != null) {
+				logger.error(exception.getMessage());
+				Thread.sleep(2000);
+			} else {
+				if (connectFuture.isConnected()) {
+					logger.info("Connected successfully");
+					session = connectFuture.getSession();
+					isConnect = true;
+				}
+			}
+		}
+		if (!isConnect) {
+			logger.warn("Connect failed");
+		}
+
+	}
+	public static synchronized void sendMsg(BaseMsg msg) {
+		if (session != null && session.isConnected()) {
+			session.write(msg);
+		}
+	}
+}
