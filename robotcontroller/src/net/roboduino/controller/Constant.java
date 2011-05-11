77b30e6ca095a557ceff2263ee79d5ca8173699e
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-05-11 12:55:28 +0000

    去掉控制端蓝牙功能

---
+++ robotcontroller/src/net/roboduino/controller/Constant.java
@@ -0,0 +1,7 @@
+package net.roboduino.controller;
+
+public class Constant {
+	 // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String DEFAULT_DEVICE_ADDRESS = "00:06:66:03:73:7B";
+}
