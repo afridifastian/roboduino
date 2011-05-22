7a3e94fa20816ce36f7ac5eabad822378e9d5425
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-05-22 05:18:07 +0000

    加上远程视频的功能

---
+++ commons/src/net/roboduino/commons/model/IBaseRobot.java
@@ -0,0 +1,13 @@
+package net.roboduino.commons.model;
+
+public interface IBaseRobot {
+
+	public abstract void speedUp();
+
+	public abstract void speedDown();
+
+	public abstract void turnLeft();
+
+	public abstract void turnRight();
+	public abstract String stop();
+}
