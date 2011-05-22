61c87745a63d02a39dc8d847f6b00be92fa8b4c7
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-05-22 05:17:27 +0000

    加上远程视频的功能

--- trunk/robotcontroller/src/net/roboduino/controller/CommandUtil.java
+++ trunk/robotcontroller/src/net/roboduino/controller/CommandUtil.java
@@ -5,8 +5,8 @@ import net.roboduino.controller.socket.TCPClient;
 import android.content.Context;
 
 public class CommandUtil {
-//	private static final Logger logger = LoggerFactory
-//			.getLogger(CommandUtil.class);
+	// private static final Logger logger = LoggerFactory
+	// .getLogger(CommandUtil.class);
 
 	/** 驱动部分 Drive Motor Power Mode */
 	public static String driveMotorP(Context context, String address, byte m1p,
@@ -16,8 +16,7 @@ public class CommandUtil {
 	}
 
 	/** 驱动部分 Drive Motor Speed Mode */
-	public static String driveMotorS(Context context, String address, byte m1s,
-			byte m2s) {
+	public static String driveMotorS(Context context, byte m1s, byte m2s) {
 		return TCPClient.sendMsg(ProtocolConstant.MSG_CMD_MOTOR_SPEED,
 				new byte[] { m1s, m2s });
 	}
