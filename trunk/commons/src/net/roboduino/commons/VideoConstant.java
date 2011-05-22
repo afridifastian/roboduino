cdece1b8e7021207e8cad7a6eb2cb0970e3b1a82
jiangguilong2000@gmail.com <jiangguilong2000@gmail.com@4f242f06-2bba-d0f2-32ac-deb01479875c>
2011-05-22 05:18:07 +0000

    加上远程视频的功能

--- trunk/commons/src/net/roboduino/commons/VideoConstant.java
+++ trunk/commons/src/net/roboduino/commons/VideoConstant.java
@@ -4,5 +4,7 @@ public class VideoConstant {
 	public static final int WIDTH = 240;
 	public static final int HEIGHT = 160;
 	public static final int SIZE = WIDTH*HEIGHT;
+	public static final int INTERVAL=300;//毫秒级
+	public static long TIME=0;
 	
 }
